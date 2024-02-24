ef674e45775880bf629eb0306c56bf0937922d46
Leidy Castaño <leidy.castanoc@udea.edu.co>
2024-02-24 15:46:18 -0500

    mostrarAnimacion

--- reverse.c
+++ reverse.c
@@ -8,3 +8,20 @@ int main(int argc, char *argv[]) {
     limpiarPantalla();
     return 0;
 }
+
+void mostrarAnimacion() {
+    const char listacaracteres[] = "|/-\\";       //".oOo."     "|/-\\"
+    const int numCaracteres = sizeof(listacaracteres) - 1;
+
+    printf("Iniciando con sistemas operativos:  ");
+    fflush(stdout);
+
+    for (int i = 0; i < 8; ++i) {
+        printf("\b%c", listacaracteres[i % numCaracteres]);
+        fflush(stdout);                              //Forza la escritura de caracteres  que se encuentran en un buffer o flujo de salida
+        sleep(1);  // Pausa de 1 segundo
+    }
+
+    printf("\b \n");  // Limpiar el último carácter
+    fflush(stdout);                                //Saca caracter de retroce esto nos ayuda en dar la ilusíon de movimiento
+}
