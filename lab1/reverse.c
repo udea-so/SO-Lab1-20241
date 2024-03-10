89c61e3763bf3e5497d942dbb69929dd30f92762
Felipe Cadavid <felipe.cadavidr@udea.edu.co>
2024-03-10 18:06:07 -0500

    fixed some warnings

--- lab1/reverse.c
+++ lab1/reverse.c
@@ -4,34 +4,66 @@ a star, and may wrap over lines */
 // two slashes work too (and may be preferred)
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 
 void reverseText(FILE *inputFile, FILE *outputFile, bool *withOutput) {
-    const MAX_LENGTH = 256;
-    char line = buffer[MAX_LENGTH];
+    const int MAX_LENGTH = 256;
+    char line[MAX_LENGTH];
 
     while(fgets(line, MAX_LENGTH, inputFile) != NULL){
         int lineLength = strlen(line);
 
-        for(int i = lineLength -1, i >= 0; i--){
+        for(int i = lineLength -1; i >= 0; i--){
             if(withOutput){
-                fputc(line[i], outputFile)
+                fputc(line[i], outputFile);
             } else {
-                printf("%s", line[i])
+                printf("%s", line[i]);
             }
             
         }
     }
 
+
+}
+
+FILE *openFile(char *filename, char *mode) {
+    FILE *file = fopen(filename, mode);
+    if (file == NULL) {
+        fprintf(stderr, "reverse: cannot open file '/no/such/file.txt'\n");
+        exit(1);
+    }
+    return file;
 }
 
 // main returns an integer
 int main(int argc, char *argv[])
 {
-    /* printf is our output function;
-    by default, writes to standard out */
-    /* printf returns an integer, but we ignore that */
-    printf("hello, world\n");
-    /* return 0 to indicate all went well */
-    return (0);
+    FILE *input = NULL;   
+    FILE *output = stdout;
+
+    switch(argc){
+        case 1: //Case if ./reverse
+            printf("Enter the file location, then press CTRL + D");
+            input = stdin;
+            reverseText(input, output, false);
+            exit(0);
+        break;
+        case 2: //Case if ./reverse input.txt
+            input = openFile(argv[1], "r");
+            reverseText(input, output, false);
+            exit(0);
+        break;
+        case 3: //Case if ./reverse input.txt output.txt
+            input = openFile(argv[1], "r");
+            output = openFile(argv[2], "w");
+            reverseText(input, output, true);
+            exit(0);
+        break;
+        default: //If more than 3
+            fprintf(stderr, "usage: reverse <input> <output>\n");
+            exit(1);
+    }
+
 }
