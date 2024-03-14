ea4b930af0c4ec1fdc8f068c3d7a437d5c157511
Jaime Alexis Herrera R <jalexis.herrera@udea.edu.co>
2024-03-13 23:06:49 -0500

    add method compareFileNames, hardlink and main

--- reverse.c
+++ reverse.c
@@ -1,8 +1,19 @@
+/* header files go up here */
+/* note that C comments are enclosed within a slash and
+a star, and may wrap over lines */
+// two slashes work too (and may be preferred)
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
+/*
+This method is the one that reverses the text line by line.
+It takes the input and passes it through a buffer, reads each line and then prints it in reverse
+*/
 void reverseText(FILE *input, FILE *output, bool withOutput) {
     char **lines = NULL;
     char *buffer = NULL;
@@ -19,12 +30,13 @@ void reverseText(FILE *input, FILE *output, bool withOutput) {
 
     // Print lines in reverse order to output file or stdout
     for (size_t i = line_count; i > 0; i--) {
-        if (withOutput) {
+        if(withOutput){
             fprintf(output, "%s", lines[i - 1]);
             free(lines[i - 1]);
         } else {
-            printf("%s", lines[i - 1]);
+            fprintf(stdout, "%s", lines[i - 1]);
         }
+        
     }
 
     // Free remaining allocated memory
@@ -32,43 +44,74 @@ void reverseText(FILE *input, FILE *output, bool withOutput) {
     free(buffer);
 }
 
+
+/*
+Compares the files making sure that both files are working correctly
+returns 1 if both file names are equal
+*/
+int compareFileNames(const char *file1, const char *file2) {
+    return strcmp(file1, file2) == 0;
+}
+
+
+/*
+Validates that a file isn't hard linked
+return 1 if there if the number of hardlinks is greater than one
+*/
+int isHardlinked(const char *filename) {
+    struct stat fileInfo;
+    return fileInfo.st_nlink > 1;
+}
+
+
+/*
+Attempts to open a file and if the file is null or doesn't exist it exit with an error
+*/
 FILE *openFile(char *filename, char *mode) {
     FILE *file = fopen(filename, mode);
     if (file == NULL) {
-        fprintf(stderr, "reverse: cannot open file '%s'\n", filename);
+        fprintf(stderr, "reverse: cannot open file '/no/such/file.txt'\n");
         exit(1);
     }
     return file;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc > 3) {
-        fprintf(stderr, "usage: reverse <input> <output>\n");
-        exit(1);
-    }
+/*
+Main method
+*/
+int main(int argc, char *argv[])
+{
+    FILE *input = NULL;   
+    FILE *output = stdout;
+    const char *file1 = argv[1];
+    const char *file2 = argv[2];
 
-    FILE *inputFile = fopen(argv[1], "r");
-    if (inputFile == NULL) {
-        fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
-        exit(1);
-    }
 
-    FILE *outputFile = stdout;
-    if (argc == 3) {
-        outputFile = fopen(argv[2], "w");
-        if (outputFile == NULL) {
-            fprintf(stderr, "reverse: cannot open file '%s'\n", argv[2]);
+    switch(argc){
+        case 1: // When the command is passed without more arguments: ./reverse
+            printf("Enter the file location, then press CTRL + D");
+            input = openFile(stdin , "r");
+            reverseText(input, output, false);
+            exit(0);
+        break;
+        case 2: //When the command is passed with the input file: ./reverse input.txt
+            input = openFile(file1, "r");
+            reverseText(input, output, false);
+            exit(0);
+        break;
+        case 3: //When the command is passed with both input and output files: ./reverse input.txt output.txt
+            input = openFile(file1 , "r");
+            output = openFile(file2, "w");
+            if(compareFileNames(file1, file2) || isHardlinked(file1)){
+                fprintf(stderr, "reverse: input and output file must differ\n");
+            }
+            reverseText(input, output, true);
+            exit(0);
+        break;
+        default: //When too many arguments are passed, meaning more than 2 files or other invalid arguments
+            fprintf(stderr, "usage: reverse <input> <output>\n");
             exit(1);
-        }
+        break;    
     }
 
-    reverseText(inputFile, outputFile, argc == 3);
-
-    fclose(inputFile);
-    if (argc == 3) {
-        fclose(outputFile);
-    }
-
-    return 0;
 }
-
