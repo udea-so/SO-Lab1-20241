10de3907cff5f138de69ffcf385f2b099293baa4
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-03-20 04:39:00 -0500

    eliminar codigo innecesario

--- reverse.c
+++ reverse.c
@@ -21,14 +21,15 @@ int sonIguales(const char *archivo1, const char *archivo2)
     return 1;
 }
 
+
+
+
 int main(int argc, char *argv[])
 {
 
     FILE *archivoDeEntrada;
     FILE *archivoDeSalida;
 
-    char textoDeEntrada[24];
-    // char textoDeEntrada2[500];
 
     //  TEST 1 --------------------
     if (argc >= 4)
@@ -40,6 +41,9 @@ int main(int argc, char *argv[])
     archivoDeEntrada = fopen(argv[1], "r");
     archivoDeSalida = fopen(argv[2], "w");
 
+
+
+
     //  TEST 2 Y 3 --------------------
     if (archivoDeEntrada == NULL || archivoDeSalida == NULL)
     {
@@ -47,7 +51,10 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    //  TEST 4 ------------------- comentar para probar el TEST 5
+
+
+
+    //  TEST 4 ------------------- 
     /*strcmp compara dos cadenas. Si son identicas retorna 0
     si son diferentes, el programa sigue corriendo normal.*/
     if (strcmp(argv[1], argv[2]) == 0)
@@ -55,6 +62,9 @@ int main(int argc, char *argv[])
         fprintf(stderr, "reverse: input and output file must differ\n");
         return 1;
     }
+
+
+
 
     //  TEST 5 --------------------
     if (sonIguales(argv[1], argv[2]) == 1)
@@ -64,56 +74,21 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+
+
+
     //  TEST 6 --------------------------
 
     int numeroDeLineas = 0;
+    char textoDeEntrada[24];
 
-    // fgets lee una linea de texto desde el archivo archivoDeEntrada
+    // FGETS lee una linea de texto desde el archivo archivoDeEntrada
     // lo guarda en un array de caracteres 'textoDeEntrada'
     /*
         Tiene los siguientes argumentos:
             ARG1 (textoDeEntrada) = El array de caracteres donde se guardará la linea leida.
             ARG2 (sizeOf(textoDeEntrada)) = El tamaño maximo de caracteres que leerá.
             ARG3 (archivoDeEntrada) = El archivo que tienen el texto que se leerá.
-    */
-    while (fgets(textoDeEntrada, sizeof(textoDeEntrada), archivoDeEntrada))
-    {
-        numeroDeLineas++;
-        // printf("%s", textoDeEntrada);
-
-        // fputs(textoDeEntrada, archivoDeSalida);
-        // numeroDeLineas++;
-    }
-    // printf("Numero de Lineas: %d\n", numeroDeLineas);
-
-    /*
-    char **apuntador1 = malloc(sizeof(char *));
-    char **apuntador2 = malloc(sizeof(char *));
-    char **apuntador3 = malloc(sizeof(char *));
-    char **apuntador4 = malloc(sizeof(char *));
-
-    *apuntador1 = malloc(sizeof(char)*50);
-    *apuntador2 = malloc(sizeof(char)*50);
-    *apuntador3 = malloc(sizeof(char)*50);
-    *apuntador4 = malloc(sizeof(char)*50);
-
-
-    fseek(archivoDeEntrada, 0, SEEK_SET);
-
-    fgets(*apuntador4, sizeof(apuntador4), archivoDeEntrada);
-    fgets(*apuntador3,  sizeof(apuntador3),archivoDeEntrada);
-    fgets(*apuntador2,  sizeof(apuntador2),archivoDeEntrada);
-    fgets(*apuntador1,  sizeof(apuntador1),archivoDeEntrada);
-
-    fputs(*apuntador1, archivoDeSalida);
-    fputs(*apuntador2, archivoDeSalida);
-    fputs(*apuntador3, archivoDeSalida);
-    fputs(*apuntador4, archivoDeSalida);
-
-    free(apuntador1);
-    free(apuntador2);
-    free(apuntador3);
-    free(apuntador4);
     */
 
     // Cuenta el numero de lineas
@@ -122,48 +97,38 @@ int main(int argc, char *argv[])
         numeroDeLineas = numeroDeLineas + 1;
     }
 
-    //int vectorApuntadores[numeroDeLineas];
+    // Punteros que puntarán a cada unaa de las lineas leidas
+    char *textoAlmacenado[numeroDeLineas];
 
-    // Crea un apuntador a un apuntador de caracteres
-    // reserva espacio de memoria suficiente para crear n de estos apuntadores
-    // donde n es numeroDeLineas
-    char **apuntadorALineas = malloc(sizeof(char *) * numeroDeLineas);
     for (int i = 0; i < numeroDeLineas; i++)
     {
-        //Cada uno de los apuntadores creados arriba, apunta
-        // a los siguientes apuntadores
-        apuntadorALineas[i] = malloc(sizeof(char) * 10);
+        textoAlmacenado[i] = malloc(sizeof(char) * 10);
     }
 
+    //Reinicia el "puntero" de archivoDeEntrada
     fseek(archivoDeEntrada, 0, SEEK_SET);
 
-    //Lee archivoDeEntrada linea por linea empezando por la última
-    // y las guarda en los espacios de memoria reservados para cada linea
+
+
+    // Lee archivoDeEntrada linea por linea empezando por la última
+    //  y las guarda en los espacios de memoria reservados para cada linea
+
     /*for (int i = numeroDeLineas - 1; i >= 0; i--)
     {
         fgets(apuntadorALineas[i], sizeof(apuntadorALineas), archivoDeEntrada);
     }*/
     for (int i = 0; i < numeroDeLineas; i++)
     {
-        fgets(apuntadorALineas[i], sizeof(apuntadorALineas), archivoDeEntrada);
+        //Se guarda en la posición de memoria reservada la linea i del archivoDeEntrada
+        fgets(textoAlmacenado[i], sizeof(textoAlmacenado), archivoDeEntrada);
     }
 
-    //Agrega al archivo de salida las lineas leidas
-    /*for (int i = 0; i < numeroDeLineas; i++)
+    // Agrega al archivo de salida las lineas leidas
+    for (int i = numeroDeLineas - 1; i >= 0; i--)
     {
-        fputs(apuntadorALineas[i], archivoDeSalida);
-        free(apuntadorALineas[i]);
-    }*/
-    for (int i = numeroDeLineas -1; i >= 0; i--)
-    {
-        fputs(apuntadorALineas[i], archivoDeSalida);
-        free(apuntadorALineas[i]);
+        fputs(textoAlmacenado[i], archivoDeSalida);
+        free(textoAlmacenado[i]);
     }
-
-    //    for(int i = 0; i < numeroDeLineas; i++){
-    //     free(apuntadorALineas[i]);
-    //    }
-    free(apuntadorALineas);
 
     // Cierra el archivo abierto
     fclose(archivoDeEntrada);
